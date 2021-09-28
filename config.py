f195b3f8b09cdb0bfa49b41f5a30abc57f022977
Moses <moses.junior@student.moringaschool.com>
2021-09-28 03:53:44 +0300

    edits to config file

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:new_password@localhost/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moses:1234@localhost/pitchbook'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
