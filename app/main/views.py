ee902286a41e58efe9bf00f98ccf5f13cfbe3d0a
Moses <moses.junior@student.moringaschool.com>
2021-09-28 03:41:36 +0300

    making main views file

---
+++ app/main/views.py
@@ -0,0 +1,25 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from ..models import User,Pitch,Comment
+from .. import db,photos
+from .forms import UpdateProfile,PitchForm,CommentForm
+from flask_login import login_required,current_user
+
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Welcome to PitchWorld'
+
+    # Getting reviews by category
+    interview_pitches = Pitch.get_pitches('interview')
+    product_pitches = Pitch.get_pitches('product')
+    promotion_pitches = Pitch.get_pitches('promotion')
+
+
+    return render_template('index.html',title = title, interview = interview_pitches, product = product_pitches, promotion = promotion_pitches)
